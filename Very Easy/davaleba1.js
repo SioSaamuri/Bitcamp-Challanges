349bbe9d3d8a250f6645cb19496f5ce7012ade1c
SioSaamuri <60820535+SioSaamuri@users.noreply.github.com>
2022-07-25 16:06:51 +0300

    Create davaleba1.js

---
+++ Very Easy/davaleba1.js
@@ -0,0 +1,12 @@
+const sia = ["abc", "d", "efghi", "j", "klmnop", "qr", "st", "uv", "wxyz"];
+console.log(sia);
+let saxeli = [sia[5][0], sia[0][0], sia[5][1], sia[2][4], sia[0][0]];
+sia.push(saxeli);
+
+let word1 = "totebs";
+let word2 = "qarisas";
+let word3 = "gadahyva marti";
+
+sia.push(word1, word2, word3);
+
+console.log(sia.length);
